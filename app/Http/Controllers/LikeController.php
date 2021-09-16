b013dbf94b94f19e96c26dd9d63c1035efef1c6b
ilhamhafidz404 <xxspanzie@gmail.com>
2021-09-16 22:01:46 +0700

    menambah toast untuk like dan unlike blog dan menambah fitur komentar dengan disqus

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Like;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class LikeController extends Controller
 {
@@ -11,8 +12,11 @@ class LikeController extends Controller
     $like = Like::whereBlog_id($blogId)->whereUser_id(auth()->user()->id)->first();
     if ($like) {
       $like->delete();
+      Alert::toast('Anda batal Menyukai Blog ini', 'info');
       return back();
     }
+    Alert::toast('Anda menyukai Blog ini', 'success');
+
     Like::create([
       'blog_id' => $blogId,
       'user_id' => auth()->user()->id
